cf6e6b363b67f39101a12c1a276da3439e2362bb
eigenmiao <eigenmiao@outlook.com>
2020-03-21 10:12:51 +0800

    Site updated: 2020-03-21 10:12:47

--- myjs/opacity.js
+++ myjs/opacity.js
@@ -1,25 +1,13 @@
 function muloading(){
-    var forediv = document.getElementById('forediv');
     var foreimg = document.getElementById('foreimg');
 
     foreimg.style.transition = "all 0.65s cubic-bezier(.5,0,1,.5)";
 
-    foreimg.style.transform = "scale(1)";
-
     if (window.innerHeight < window.innerWidth) {
-        forediv.style.height = "100%";
-        forediv.style.width = "auto";
+        foreimg.style.transform = "scale(" + window.innerHeight / 900 + ")";
 
-        foreimg.style.height = "100%";
-        foreimg.style.width = "auto";
-        foreimg.style.marginTop = "0%";
     } else {
-        forediv.style.height = "auto";
-        forediv.style.width = "100%";
-
-        foreimg.style.height = "auto";
-        foreimg.style.width = "160%";
-        foreimg.style.marginLeft = "-30%";
+        foreimg.style.transform = "scale(" + window.innerWidth / 1271 * 1.3 + ")";
     };
 
     foreimg.style.opacity = 1.0;
